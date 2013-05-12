d47c1f72d9fb1ed2278db4b8d23106b0b3d6133d
Nikhil <nikhilsaraf89@gmail.com>
2013-05-11 18:10:44 -0700

    added print warm-up script

---
+++ assignment1/nikhilsaraf_print.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import urllib
+import json
+
+
+for page in range(1,11):
+	raw_response = urllib.urlopen("http://search.twitter.com/search.json?q=Twitter&page=" + str(page))
+	json_object = json.load(raw_response)
+	print "Page: " + str(page)	
+	for tweet in json_object['results']:
+		print tweet['text']
+		print ""
