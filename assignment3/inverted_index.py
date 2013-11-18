262b42f133399f69ad189617e89de71444d69879
Nikhil <nikhilsaraf89@gmail.com>
2013-11-17 16:51:06 -0800

    corrected solution after first run. fixed incorrect assumptions, now works as expected

--- assignment3/inverted_index.py
+++ assignment3/inverted_index.py
@@ -6,24 +6,35 @@ import sys
 # mr - MapReduce object
 # data - json object formatted as a string
 def mapper(mr, data):
-    data = json.loads(dataline, encoding='latin-1')
-    values = data.text.split()
-    wordSet = set(values)
-    for word in values:
-        # output (key, value) pair (only for mapper)
-        mr.emit_intermediate(word, data.document_id)
+    # we need to treat the data as a dictionary, so we interpret it as json, this
+    # converting it to a dictionary, making it more useful
+    data = json.loads(data, encoding='latin-1')
+
+    # save document_id for code readability
+    # keys() gives an array as the input is a json dictionary,
+    # we only want the first value as a mapper will only have one key
+    document_id = data.keys()[0]
+    
+    # data's values contains an array of the words (tested via print statement)
+    # so we only need to put the values in a set to complete the preparation of it
+    # so covert the values to a set to remove duplicates
+    wordSet = set(data.values()[0])
+    for word in wordSet:
+        # we will have uniqueue words here so we can just do an emit_intermediate
+        mr.emit_intermediate(word, document_id)
 
 # Reduce function
 # mr - MapReduce object
 # key - key generated from map phse, associated to list_of_values
 # list_of_values - values generated from map phase, associated to key
 def reducer(mr, key, list_of_values):
-    # output item (only for reducer)
-    mr.emit(list_of_values)
+    # output list of values as a tuple, as specified in the question
+    # this is just an identity function
+    mr.emit({key: list_of_values})
 
 def main():
     # Assumes first argument is a file of json objects formatted as strings, 
-    #one per line.
+    # one per line.
     MapReduce.execute(open(sys.argv[1]), mapper, reducer)
 
 if __name__ == '__main__':
